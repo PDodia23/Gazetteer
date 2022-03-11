a857433c59e36208faaac5179649d3491a3dd318
PoojaDodia <65901814+PDodia23@users.noreply.github.com>
2022-03-11 15:55:17 +0000

    Create getWebcams.php

---
+++ libs/php/mapMarkers/getWebcams.php
@@ -0,0 +1,26 @@
+<?php
+
+	$url='http://api.geonames.org/searchJSON?country=' . $_REQUEST['country'] . '&featureCode=airp&maxRows=30&username=pdodia23' ;
+
+$url = "https://api.windy.com/api/webcams/v2/list/country=" .$_REQUEST["country"]. "?show=webcams:location,player&key=6mV9LRaACbm6Bpqf9fSI21labPzLhWkS";
+
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL,$url);
+
+$result=curl_exec($ch);
+
+curl_close($ch);
+
+$decode = json_decode($result,true);
+
+$output['status']['code'] = "200";
+$output['status']['name'] = "ok";
+$output['status']['description'] = "success";
+$output['data'] = $decode;
+
+header('Content-Type: application/json; charset=UTF-8');
+
+echo json_encode($output); 
